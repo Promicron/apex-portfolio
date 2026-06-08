176125cbb68112169f31d3cb4b3f3b65588700f7
Promicron <ileriodus@gmail.com.com>
2026-06-08 18:29:55 +0100

    Feat: updated skills and projects

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -21,12 +21,12 @@ const projects = [
     },
     {
       id: 3,
-      title: 'Age Predicton App',
-      description: 'A web application that predicts a person\'s age based on their facial image using Convolutional Neural Networks (CNN) and computer vision techniques.',
-      image: '/images/face.png',
-      technologies: ['HTML', 'Python', 'CSS', 'Flask', 'OpenCV'],
-      github: 'https://github.com/Promicron/estmage',
-      live: '#',
+      title: 'CHOMP_PY',
+      description: 'A python implementation of Covariant Hamiltonian Optimization Method, a numerical method for solving the time-independent Schrödinger equation.',
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
+      technologies: ['Python', 'PyBullet', 'Numpy', 'Matplotlib'],
+      github: 'https://github.com/Promicron/CHOMP_PY',
+      live: '',
       featured: false
     },
     {
@@ -41,14 +41,47 @@ const projects = [
     },
     {
       id: 5,
-      title: 'Quiz App',
-      description: 'An interactive quiz application that allows users to take quizzes on various topics, track their scores, and improve their knowledge through engaging questions.',
-      image: '/images/promicle2.png',
-      technologies: ['React', 'Bootstrap CSS', ''],
-      github: '#',
-      live: 'https://promicle.vercel.app',
+      title: 'Loqui',
+      description: 'A voice-first diary app that allows users to record their thoughts and feelings in audio format, and transcribes them into text. Its a cross platform app that.',
+      image: '/images/loqui-img.jpeg',
+      technologies: ['React Native', 'Nativewind CSS', 'React-Native-Voice', 'Groq AI', 'Expo', 'Typescript'],
+      github: 'https://github.com/Promicron/loqui',
+      live: '#',
+      featured: true
+    },
+    {
+      id: 6,
+      title: 'IntFootball',
+      description: 'A predictive analysis pipeline that forecasts outcomes of international football matches.',
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
+      technologies: ['Python'],
+      github: 'https://github.com/Promicron/IntFootball',
+      live: '#',
       featured: false
     },
+    {
+      id: 7,
+      title: 'Age Predicton App',
+      description: 'A web application that predicts a person\'s age based on their facial image using Convolutional Neural Networks (CNN) and computer vision techniques.',
+      image: '/images/face.png',
+      technologies: ['HTML', 'Python', 'CSS', 'Flask', 'OpenCV'],
+      github: 'https://github.com/Promicron/estmage',
+      live: '#',
+      featured: false
+    },
+    
+
+    // {
+    //   id: 5,
+    //   title: 'Quiz App',
+    //   description: 'An interactive quiz application that allows users to take quizzes on various topics, track their scores, and improve their knowledge through engaging questions.',
+    //   image: '/images/promicle2.png',
+    //   technologies: ['React', 'Bootstrap CSS', ''],
+    //   github: '#',
+    //   live: 'https://promicle.vercel.app',
+    //   featured: false
+    // },
+
     
   ];
 export default projects;
--- src/assets/skills.js
+++ src/assets/skills.js
@@ -3,6 +3,8 @@ const skills = [
     { name: 'JavaScript', level: 90, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' },
     { name: 'Python', level: 85, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg' },
     { name: 'CSS/SCSS', level: 92, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg' },
+    { name: 'TypeScript', level: 85, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg' },
+    {name: 'React Native', level: 70, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg'},
     { name: 'Node.js', level: 80, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg' },
     { name: 'Git', level: 88, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg' },
     { name: 'MongoDB', level: 70, icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg' },
